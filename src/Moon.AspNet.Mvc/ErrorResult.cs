1392e697437bfaabf7bc72a158551c0c5c32eb5b
Dusan Janosik <dusan@djanosik.cz>
2015-09-23 16:45:37 +0200

    Adds a result for returning error messages to clients.

---
+++ src/Moon.AspNet.Mvc/ErrorResult.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Moon.AspNet.Mvc
+{
+    /// <summary>
+    /// An <see cref="ErrorResult" /> that when executed will produce a Server Error (500) response.
+    /// </summary>
+    public class ErrorResult : ObjectResult
+    {
+        /// <summary>
+        /// Creates a new <see cref="BadRequestObjectResult" /> instance.
+        /// </summary>
+        /// <param name="errorMessage">The error message to be returned to the client.</param>
+        public ErrorResult(string errorMessage)
+            : base(new { Error = errorMessage })
+        {
+            Requires.NotNullOrWhiteSpace(errorMessage, nameof(errorMessage));
+
+            StatusCode = 500;
+        }
+    }
+}
